119c21a558ce2ca3c559d2e9fea522b1f952e766
Zachary Scherer <californiazds@gmail.com>
2020-02-05 20:39:29 -0700

    Initial Upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Zachary Scherer Portfilio</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+<link rel="stylesheet" type="text/css" href="css/home.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<div class="container-fluid w3-dark-grey" id="header">
+    <h1 class="headerText">Welcome to Zachary Scherer's Portfilio</h1>
+    <p class="text-warning">This site is still being worked on</p>
+</div>
+<div id="hLinks" class="w3-bar w3-indigo">
+        <a href="#" class="w3-bar-item w3-button w3-hover-blue">placeholder</a>
+        <a href="#" class="w3-bar-item w3-button w3-hover-blue">placeholder</a>
+        <a href="#" class="w3-bar-item w3-button w3-hover-blue">placeholder</a>
+</div>
+
+<div class="grid-container">
+<div class="links content">
+    <h3>Find me here:</h3>
+    <ul>
+    <li><p><a class="link" href="https://baylor.joinhandshake.com/users/18651126">Handshake</a></p></li>
+    <li><p><a class="link" href="https://github.com/ZSlongtalk">Github</a></p></li>
+    </ul>
+</div>
+<div class="main content">
+    <div class="jumbotron">
+        <h3>Hello my name is Zachary Scherer</h3>
+        <p>I am a Baylor Computer Science student working on my undergrade</p>
+    </div>
+</div>
+<div class="footer content">
+    <div id="StudentOrg">
+        <p><b>Student Organizations/Past Experience</b></p>
+        <table>
+            <tr>
+                <th>Centennial High School</th>
+                <th>Baylor University</th>
+            </tr>
+            <tr>
+                <td>Bussiness Professionals Of America (BPA)</td>
+                <td>Association of Computing Machinary (ACM)</td>
+            </tr>
+            <tr>
+                <td>National Honor Society (NHS)</td>
+                <td>Computing For Compassion (C4C)</td>
+            </tr>
+            <tr>
+                <td><a href="https://github.com/ZSlongtalk/WDT2018_DC">BPA Web Site Design Team</a></td>
+                <td><a href="https://github.com/ZSlongtalk/VideoGameGroupProject">Intro 1 (1430) group project</a></td>
+            </tr>
+        </table>
+    </div>
+</div>
+</div>
+
+</body>
+
+</html>
