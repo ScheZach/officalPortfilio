2a58ab4a7608f61c93e3302c245bf160f93aa458
Zachary Scherer <californiazds@gmail.com>
2020-02-11 15:35:03 -0700

    updated home page

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 <body>
 <div class="container-fluid w3-dark-grey" id="header">
     <h1 class="headerText">Welcome to Zachary Scherer's Portfilio</h1>
-    <p class="text-warning">This site is still being worked on</p>
+    <p class="text-warning">This site is still in progress</p>
 </div>
 <div id="hLinks" class="w3-bar w3-indigo">
         <a href="#" class="w3-bar-item w3-button w3-hover-blue">placeholder</a>
@@ -33,8 +33,19 @@
 </div>
 <div class="main content">
     <div class="jumbotron">
+        <img id="headShot" src="img/Headshot1.jpg" alt="headShot"></img>
         <h3>Hello my name is Zachary Scherer</h3>
         <p>I am a Baylor Computer Science student working on my undergrade</p>
+        <p>I am from Boise, ID</p>
+        <p>I worked with: </p>
+        <ul>
+            <li>C++ (Baylor CSI 1430/1440)</li>
+            <li>HTML/CSS (Centennial High School CTE)</li>
+            <li>JavaScript/JQuery (Centennial High School CTE)</li>
+            <li>W3.css/Bootstrap 4 (Centennial High School CTE)</li>
+            <li>PHP (Centennial High School CTE)</li>
+            <li>SQL (Centennial High School CTE)</li>
+        </ul>
     </div>
 </div>
 <div class="footer content">
@@ -54,8 +65,8 @@
                 <td>Computing For Compassion (C4C)</td>
             </tr>
             <tr>
-                <td><a href="https://github.com/ZSlongtalk/WDT2018_DC">BPA Web Site Design Team</a></td>
-                <td><a href="https://github.com/ZSlongtalk/VideoGameGroupProject">Intro 1 (1430) group project</a></td>
+                <td><a class="tableLink" href="https://github.com/ZSlongtalk/WDT2018_DC">BPA Web Site Design Team</a></td>
+                <td><a class="tableLink" href="https://github.com/ZSlongtalk/VideoGameGroupProject">Intro 1 (1430) group project</a></td>
             </tr>
         </table>
     </div>
